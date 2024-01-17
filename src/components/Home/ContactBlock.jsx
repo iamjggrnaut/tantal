e21477932dd0f88a0b4e84aec37e5f1cb582c865
Rustam A <85253947+iamjggrnaut@users.noreply.github.com>
2024-01-18 01:30:20 +0300

    Update ContactBlock.jsx

--- src/components/Home/ContactBlock.jsx
+++ src/components/Home/ContactBlock.jsx
@@ -47,7 +47,7 @@ const ContactBlock = () => {
                     </div>
 
                     <div>
-                        <MapContainer center={position} zoom={10} scrollWheelZoom={true}>
+                        <MapContainer center={position} zoom={11} scrollWheelZoom={true}>
                             <TileLayer
                                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
